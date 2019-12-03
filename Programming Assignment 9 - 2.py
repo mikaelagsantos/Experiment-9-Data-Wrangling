4691ee99dbea0a7ae53d445b2c38bd2a5c8ecfcb
Mikaela G. Santos <57312908+mikaelagsantos@users.noreply.github.com>
2019-12-03 13:10:18 +0800

    Add files via upload

---
+++ Programming Assignment 9 - 2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+box = pd.DataFrame(
+        {'Box' : ['Box1','Box1','Box1','Box2','Box2','Box2'],
+         'Dimension' : ['Length','Width','Height','Length','Width','Height'],
+         'Value' : [6,4,2,5,3,4] })
+
+tidy = box.pivot_table(index = 'Box', columns = 'Dimension', values = 'Value')
+
+volume = [48, 60]
+
+tidy['Volume'] = volume
